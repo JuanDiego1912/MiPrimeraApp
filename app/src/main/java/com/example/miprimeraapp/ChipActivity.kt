f724b9cd53a035e70e7f9a876db985611e43628e
JuanDiego1912 <juandiegorondon1999@gmail.com>
2024-10-22 20:32:23 +0200

    Dia 22/10/24

---
+++ app/src/main/java/com/example/miprimeraapp/ChipActivity.kt
@@ -0,0 +1,29 @@
+package com.example.miprimeraapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+
+class ChipActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_chip)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        var cgAvisos = findViewById<ChipGroup>(R.id.cgAvisos);
+
+        for (i in 0..<cgAvisos.childCount) {
+            val chip = cgAvisos.getChildAt(i) as Chip
+        }
+
+    }
+}
