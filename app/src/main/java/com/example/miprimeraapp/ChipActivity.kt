65eddd0d30165126619bedd548c3e43103ab9c04
JuanDiego1912 <juandiegorondon1999@gmail.com>
2024-10-23 18:20:34 +0200

    Dia 23/10/24

--- app/src/main/java/com/example/miprimeraapp/ChipActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/ChipActivity.kt
@@ -1,6 +1,7 @@
 package com.example.miprimeraapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,9 +22,37 @@ class ChipActivity : AppCompatActivity() {
 
         var cgAvisos = findViewById<ChipGroup>(R.id.cgAvisos);
 
-        for (i in 0..<cgAvisos.childCount) {
+        for (i in 0..< cgAvisos.childCount) {
+            /*Casteo el hijo que he tomado de cgAvisos a Chip
+            * porque sino obtendría un objeto View que no me interesa. Cada
+            * elemento View se castea a chip y posteriormente uso sus métodos*/
             val chip = cgAvisos.getChildAt(i) as Chip
-        }
+            chip.isChecked = false
 
+            /* Cada vez que se pulse el aspa de un chip este desaperecera. It
+            * se refiere al objeto Chip pulsado*/
+            chip.setOnCloseIconClickListener {
+                cgAvisos.removeView(it)
+            }
+
+            chip.setOnClickListener {
+                /* Convierto It que es el chip exterior a esta función en
+                * otro chip auxiliar
+                * val chipAux = it as Chip
+                */
+
+                /* Si it esta marcado entonces se ejecutara*/
+                if ((it as Chip).isChecked) {
+                    /* Toast sirve para mostrar un pequeño mensaje por pantalla
+                    * con la durración, el texto y en el View que
+                    * decidamos */
+                    Toast.makeText(this,
+                        //Casteo it como un objeto Chip y obtengo su texto
+                        "Se ha pulsado ${it.text}",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
     }
 }
