3e705b257acd11aea18bdea5d3f208632e20537b
JuanDiego1912 <juandiegorondon1999@gmail.com>
2024-10-28 18:19:28 +0100

    Dia 28/10/24

--- app/src/main/java/com/example/miprimeraapp/ImageViewActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/ImageViewActivity.kt
@@ -1,10 +1,12 @@
 package com.example.miprimeraapp
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.squareup.picasso.Picasso
 
 class ImageViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,11 @@ class ImageViewActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        /* Importar una imagen desde internet
+        val ivDarthVaderRemoto = findViewById<ImageView>(R.id.darthVaderRemoto)
+        Picasso.get().load("https://noticiasnet1.cdn.net.ar/252/noticiasnet/images/01/15/79/1157976_62297041261ee982190437e0236913c7eb44ca5ed97e5c4d9daf0acc372be261/lg.jpg")
+            .into(ivDarthVaderRemoto)*/
+
     }
 }
